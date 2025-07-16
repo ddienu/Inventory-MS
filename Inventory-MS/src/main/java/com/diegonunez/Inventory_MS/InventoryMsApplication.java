356c1ddaf806199ecfd7c3a2f50af05368735864
ddienu <rockdann@hotmail.com>
2025-07-16 13:52:02 -0500

    Initial commit for spring boot project

---
+++ Inventory-MS/src/main/java/com/diegonunez/Inventory_MS/InventoryMsApplication.java
@@ -0,0 +1,13 @@
+package com.diegonunez.Inventory_MS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMsApplication.class, args);
+	}
+
+}
---
+++ Inventory-MS/src/test/java/com/diegonunez/Inventory_MS/InventoryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegonunez.Inventory_MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
